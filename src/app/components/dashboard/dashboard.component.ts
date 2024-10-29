db7f1f53025d273c555526d51f3920f6aefb6353
fpolania <lassofabian36@gmail.com>
2024-10-29 13:32:57 -0500

    se hicieron las card del dashboard

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -2,11 +2,12 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { MenuComponent } from '../../shared/menu/menu.component';
 import { GraphisComponent } from '../../shared/graphis/graphis.component';
+import { CardInfoComponent } from '../../shared/card-info/card-info.component';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [CommonModule, MenuComponent, GraphisComponent],
+  imports: [CommonModule, MenuComponent, GraphisComponent,CardInfoComponent ],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
